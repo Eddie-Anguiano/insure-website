ba5aeaa50109f2c933eed0380fd0cabc10e6ac7c
Eddie Anguiano <eddiesanguiano@gmail.com>
2020-04-04 22:09:07 -0700

    desktop css/html added

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,11 @@ import { bro } from "./bro";
 import "../styles/main.scss";
 
 console.log(bro("dude"));
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".header__nav-mobile");
+
+hamburger.addEventListener("click", e => {
+  hamburger.classList.toggle("hamburger--isActive");
+  nav.classList.toggle("header__nav-mobile--isActive");
+});
