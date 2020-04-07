9e6c88bf08befb8e0c63d9d42828dbf446fc2b9e
Eddie Anguiano <eddiesanguiano@gmail.com>
2020-04-06 21:03:58 -0700

    added fadeIN scroll events

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,21 @@ hamburger.addEventListener("click", e => {
   hamburger.classList.toggle("hamburger--isActive");
   nav.classList.toggle("header__nav-mobile--isActive");
 });
+
+window.addEventListener("load", () => {
+  document.querySelector("body").classList.remove("preload");
+});
+
+const anime = document.querySelectorAll(".anime");
+
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting === true) {
+      entry.target.classList.add("animated", "fadeInDown");
+    }
+  });
+});
+
+anime.forEach(item => {
+  observer.observe(item);
+});
